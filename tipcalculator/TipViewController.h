82e1fe000eb8447e1451d06c9c945eadbc694441
sam <sam@wealthfront.com>
2014-12-27 12:13:45 -0500

    Add TipViewController, set as rootViewController

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Sam Sweeney on 12/27/14.
+//  Copyright (c) 2014 Wealthfront. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/TipViewController.m
@@ -0,0 +1,38 @@
+//
+//  TipViewController.m
+//  tipcalculator
+//
+//  Created by Sam Sweeney on 12/27/14.
+//  Copyright (c) 2014 Wealthfront. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
